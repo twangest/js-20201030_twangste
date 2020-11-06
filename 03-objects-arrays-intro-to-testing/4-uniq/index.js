973304ee36121bb9b45dc8284be3a9688ba9d301
Alexey Struchenkov <forcorp@inbox.ru>
2020-11-06 23:51:27 +0200

    Решение 4-uniq

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,10 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
+  if (typeof arr === 'undefined') {
+    return [];
+  }
+  // короткая запись
+  return [...(new Set([...arr]))];
 
 }
